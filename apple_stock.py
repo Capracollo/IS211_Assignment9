293e4bff8be44c83ad82ffb1d98e9ad31d1448d5
Yurtledaturtle <Yurtletmail@gmail.com>
2022-04-20 02:26:52 -0500

    committing adjustments to function

--- apple_stock.py
+++ apple_stock.py
@@ -1,2 +1,23 @@
-if __name__ == "__main__":
-    pass
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_prices(url):
+
+    web_page = request.get(url)
+    web_data = web_page.text
+    soup = BeautifulSoup(web_data, features='lxml')
+
+    rows = soup.find_all('tr', {'class': 'BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)'})
+
+    for row in rows:
+        x = row.find_all('td')
+        day = str(rows[0].get_text())
+        price = str(rows[4].get_text())
+
+        print(f'Date - {day}, price - {price}')
+
+    if __name__ == "__main__":
+        """Main entry point"""
+        url = "https://finance.yahoo.com/quote/AAPL/history?p=AAPL"
+        get_prices(url)
